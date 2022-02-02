7769b7c901484fb6a92da3a60ef29fcdbdf32b5a
최은정 <qhdrn67@naver.com>
2022-02-02 13:12:27 +0900

    TailwindCSS Setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,14 @@
 import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
-  return null;
+  return (
+    <div className='bg-red-500'>
+      <h1 className='text-black'>it works</h1>
+      <button className='bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3'>
+        button
+      </button>
+    </div>
+  );
 };
 
 export default Home;
