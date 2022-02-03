c21195cdc064076e66b0d5159b5610af9bf455d8
최은정 <qhdrn67@naver.com>
2022-02-03 22:44:22 +0900

    Test Drive part Two : Profile

--- pages/index.tsx
+++ pages/index.tsx
@@ -57,25 +57,55 @@ const Button = tw.div`
   text-white
 `;
 
+const Receipt = () => (
+  <Box>
+    <SelectItem>Select Item</SelectItem>
+    <Item>
+      <ItemName>Grey Chair</ItemName>
+      <ItemPrice>$19</ItemPrice>
+    </Item>
+    <Item>
+      <ItemName>Grey Chair</ItemName>
+      <ItemPrice>$19</ItemPrice>
+    </Item>
+    <TotalBox>
+      <span>Total</span>
+      <ItemName>$10</ItemName>
+    </TotalBox>
+    <Button>Checkout</Button>
+  </Box>
+);
+
+const Profile = () => (
+  <div className='overflow-hidden rounded-3xl bg-white shadow-xl'>
+    <div className='bg-blue-500 p-6 pb-14'>
+      <span className='text-2xl text-white'>Profile</span>
+    </div>
+    <div className='relative -top-5 rounded-3xl bg-white p-6'>
+      <div className='relative -top-16 flex items-end justify-between'>
+        <div className='flex flex-col items-center'>
+          <span className='text-sm text-gray-500'>Orders</span>
+          <span className='font-medium'>340</span>
+        </div>
+        <div className='h-24 w-24 rounded-full bg-red-400' />
+        <div className='flex flex-col items-center'>
+          <span className='text-sm text-gray-500'>Spent</span>
+          <span className='font-medium'>$340</span>
+        </div>
+      </div>
+      <div className='relative  -mt-10 -mb-5 flex flex-col items-center'>
+        <span className='text-lg font-medium'>Tony Molloy</span>
+        <span className='text-sm text-gray-500'>미국</span>
+      </div>
+    </div>
+  </div>
+);
+
 const Home: NextPage = () => {
   return (
     <Container>
-      <Box>
-        <SelectItem>Select Item</SelectItem>
-        <Item>
-          <ItemName>Grey Chair</ItemName>
-          <ItemPrice>$19</ItemPrice>
-        </Item>
-        <Item className='my-0'>
-          <ItemName>Grey Chair</ItemName>
-          <ItemPrice>$19</ItemPrice>
-        </Item>
-        <TotalBox>
-          <span>Total</span>
-          <ItemName>$10</ItemName>
-        </TotalBox>
-        <Button>Checkout</Button>
-      </Box>
+      <Receipt />
+      <Profile />
     </Container>
   );
 };
