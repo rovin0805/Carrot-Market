881c2d11e1c19322f294147488d4b433f478d8b4
최은정 <qhdrn67@naver.com>
2022-02-17 10:51:27 +0900

    [Auth] Accounts Logic

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,8 +1,21 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import withHandler from '@libs/server/withHandler';
+import client from '@libs/server/client';
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-  console.log('REQ', req.body);
+  const { phone, email } = req.body;
+  const payload = phone ? { phone: +phone } : { email };
+  const user = await client.user.upsert({
+    where: {
+      ...payload,
+    },
+    create: {
+      name: 'Anonymous',
+      ...payload,
+    },
+    update: {},
+  });
+  console.log('@', user);
   return res.json({ ok: true });
 }
 
--- pages/enter.tsx
+++ pages/enter.tsx
@@ -25,8 +25,9 @@ const Enter: NextPage = () => {
     setMethod('phone');
   };
 
-  const onValid = (data: EnterForm) => {
-    enter(data);
+  const onValid = (validForm: EnterForm) => {
+    if (loading) return;
+    enter(validForm);
   };
 
   return (
@@ -41,10 +42,9 @@ const Enter: NextPage = () => {
                 'border-b-2 pb-4 text-sm font-medium',
                 method === 'email'
                   ? ' border-orange-500 text-orange-400'
-                  : 'border-transparent text-gray-500 hover:text-gray-400'
+                  : 'border-transparent text-gray-500 hover:text-gray-400',
               )}
-              onClick={onEmailClick}
-            >
+              onClick={onEmailClick}>
               Email
             </button>
             <button
@@ -52,18 +52,16 @@ const Enter: NextPage = () => {
                 'border-b-2 pb-4 text-sm font-medium',
                 method === 'phone'
                   ? ' border-orange-500 text-orange-400'
-                  : 'border-transparent text-gray-500 hover:text-gray-400'
+                  : 'border-transparent text-gray-500 hover:text-gray-400',
               )}
-              onClick={onPhoneClick}
-            >
+              onClick={onPhoneClick}>
               Phone
             </button>
           </div>
         </div>
         <form
           onSubmit={handleSubmit(onValid)}
-          className='mt-8 flex flex-col space-y-4'
-        >
+          className='mt-8 flex flex-col space-y-4'>
           {method === 'email' ? (
             <Input
               register={register('email')}
@@ -83,9 +81,11 @@ const Enter: NextPage = () => {
               required
             />
           ) : null}
-          {method === 'email' ? <Button text={'Get login link'} /> : null}
+          {method === 'email' ? (
+            <Button text={loading ? 'Loading' : 'Get login link'} />
+          ) : null}
           {method === 'phone' ? (
-            <Button text={'Get one-time password'} />
+            <Button text={loading ? 'Loading' : 'Get one-time password'} />
           ) : null}
         </form>
 
@@ -104,8 +104,7 @@ const Enter: NextPage = () => {
                 className='h-5 w-5'
                 aria-hidden='true'
                 fill='currentColor'
-                viewBox='0 0 20 20'
-              >
+                viewBox='0 0 20 20'>
                 <path d='M6.29 18.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0020 3.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.073 4.073 0 01.8 7.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 010 16.407a11.616 11.616 0 006.29 1.84' />
               </svg>
             </button>
@@ -114,8 +113,7 @@ const Enter: NextPage = () => {
                 className='h-5 w-5'
                 aria-hidden='true'
                 fill='currentColor'
-                viewBox='0 0 20 20'
-              >
+                viewBox='0 0 20 20'>
                 <path
                   fillRule='evenodd'
                   d='M10 0C4.477 0 0 4.484 0 10.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0110 4.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.203 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.942.359.31.678.921.678 1.856 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0020 10.017C20 4.484 15.522 0 10 0z'
