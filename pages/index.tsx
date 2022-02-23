abfb6daa5aff0ebdc1c5b5553af3ae3e593c9f3c
Choi Eun Jeong <qhdrn67@naver.com>
2022-02-23 21:58:46 +0900

    [PRODUCTS] Favorite Products

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,7 +24,6 @@ const Home: NextPage = () => {
             key={product.id}
             title={product.name}
             price={product.price}
-            comments={1}
             hearts={1}
           />
         ))}
