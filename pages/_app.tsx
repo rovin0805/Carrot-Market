e35d13123f65d756e491815bdfda2342b4081045
rovin0805 <qhdrn67@naver.com>
2022-03-21 11:51:34 +0900

    [PROFILE] Edit Profile part One

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,9 @@ import type { AppProps } from 'next/app';
 import Seo from '@components/seo';
 import { SWRConfig } from 'swr';
 
-const fetcher = (url: string) => fetch(url).then((res) => res.json());
+// TODO: protect all pages by using useUser Hook expect enter page
+
+const fetcher = (url: string) => fetch(url).then(res => res.json());
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
--- pages/profile/edit.tsx
+++ pages/profile/edit.tsx
@@ -2,17 +2,47 @@ import type { NextPage } from 'next';
 import Button from '@components/button';
 import Input from '@components/input';
 import Layout from '@components/layout';
+import useUser from '@libs/client/useUser';
+import { useForm } from 'react-hook-form';
+import { useEffect } from 'react';
+
+interface EditProfileForm {
+  email?: string;
+  phone?: string;
+  formErrors?: string;
+}
 
 const EditProfile: NextPage = () => {
+  const { user } = useUser();
+  const {
+    register,
+    setValue,
+    handleSubmit,
+    setError,
+    formState: { errors },
+  } = useForm<EditProfileForm>();
+
+  useEffect(() => {
+    if (user?.email) setValue('email', user.email);
+    if (user?.phone) setValue('phone', user.phone);
+  }, [user, setValue]);
+
+  const onValid = ({ email, phone }: EditProfileForm) => {
+    if (email === '' && phone === '') {
+      setError('formErrors', {
+        message: 'Email OR Phone number are required. You need to choose one.',
+      });
+    }
+  };
+
   return (
     <Layout canGoBack title='Edit Profile'>
-      <form className='space-y-4 py-10 px-4'>
+      <form onSubmit={handleSubmit(onValid)} className='space-y-4 py-10 px-4'>
         <div className='flex items-center space-x-3'>
           <div className='h-14 w-14 rounded-full bg-slate-500' />
           <label
             htmlFor='picture'
-            className='cursor-pointer rounded-md border border-gray-300 py-2 px-3 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:ring-2 focus:ring-orange-500 focus:ring-offset-2'
-          >
+            className='cursor-pointer rounded-md border border-gray-300 py-2 px-3 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:ring-2 focus:ring-orange-500 focus:ring-offset-2'>
             Change
             <input
               id='picture'
@@ -22,14 +52,26 @@ const EditProfile: NextPage = () => {
             />
           </label>
         </div>
-        <Input required label='Email address' name='email' type='email' />
         <Input
-          required
+          register={register('email')}
+          required={false}
+          label='Email address'
+          name='email'
+          type='email'
+        />
+        <Input
+          register={register('phone')}
+          required={false}
           label='Phone number'
           name='phone'
           type='number'
           kind='phone'
         />
+        {errors.formErrors ? (
+          <span className='my-2 block text-center font-medium text-red-500'>
+            {errors.formErrors.message}
+          </span>
+        ) : null}
         <Button text='Update profile' />
       </form>
     </Layout>
