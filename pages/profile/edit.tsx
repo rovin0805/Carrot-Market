dc21759d3b109bbb90817fadd2ef9e614a57170c
Choi Eun Jeong <qhdrn67@naver.com>
2022-03-22 22:52:11 +0900

    [CLOUDFLARE IMAGES] Profile Avatar Preview

--- pages/profile/edit.tsx
+++ pages/profile/edit.tsx
@@ -4,13 +4,14 @@ import Input from '@components/input';
 import Layout from '@components/layout';
 import useUser from '@libs/client/useUser';
 import { useForm } from 'react-hook-form';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import useMutation from '@libs/client/useMutation';
 
 interface EditProfileForm {
   email?: string;
   phone?: string;
   name?: string;
+  avatar?: FileList;
   formErrors?: string;
 }
 
@@ -27,9 +28,20 @@ const EditProfile: NextPage = () => {
     handleSubmit,
     setError,
     formState: { errors },
+    watch,
   } = useForm<EditProfileForm>();
   const [editProfile, { loading, data }] =
     useMutation<EditProfileResponse>('/api/users/me');
+  const avatar = watch('avatar');
+  const [avatarPreview, setAvatarPreview] = useState('');
+
+  useEffect(() => {
+    if (avatar && avatar.length > 0) {
+      const file = avatar[0];
+      const previewUrl = URL.createObjectURL(file);
+      setAvatarPreview(previewUrl);
+    }
+  }, [avatar]);
 
   useEffect(() => {
     if (user?.name) setValue('name', user.name);
@@ -43,7 +55,7 @@ const EditProfile: NextPage = () => {
     }
   }, [data, setError]);
 
-  const onValid = ({ email, phone, name }: EditProfileForm) => {
+  const onValid = ({ email, phone, name, avatar }: EditProfileForm) => {
     if (loading) return;
     if (email === '' && phone === '' && name === '') {
       return setError('formErrors', {
@@ -57,12 +69,21 @@ const EditProfile: NextPage = () => {
     <Layout canGoBack title='Edit Profile'>
       <form onSubmit={handleSubmit(onValid)} className='space-y-4 py-10 px-4'>
         <div className='flex items-center space-x-3'>
-          <div className='h-14 w-14 rounded-full bg-slate-500' />
+          {avatarPreview ? (
+            <img
+              src={avatarPreview}
+              className='h-14 w-14 rounded-full bg-slate-500'
+            />
+          ) : (
+            <div className='h-14 w-14 rounded-full bg-slate-500' />
+          )}
           <label
             htmlFor='picture'
-            className='cursor-pointer rounded-md border border-gray-300 py-2 px-3 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:ring-2 focus:ring-orange-500 focus:ring-offset-2'>
+            className='cursor-pointer rounded-md border border-gray-300 py-2 px-3 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:ring-2 focus:ring-orange-500 focus:ring-offset-2'
+          >
             Change
             <input
+              {...register('avatar')}
               id='picture'
               type='file'
               className='hidden'
