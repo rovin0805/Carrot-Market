c0f5beb18d44b09768116632555ed76c8c6b9e60
Choi Eun Jeong <qhdrn67@naver.com>
2022-03-22 23:22:04 +0900

    [CLOUDFLARE IMAGES] Cloudflare Setup

---
+++ pages/api/files.ts
@@ -0,0 +1,23 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import withHandler, { ResponseType } from '@libs/server/withHandler';
+import client from '@libs/server/client';
+import { withApiSession } from '@libs/server/withSession';
+
+// Upload images to Cloudflare
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
+  res.json({
+    ok: true,
+    url: '',
+  });
+}
+
+export default withApiSession(
+  withHandler({
+    methods: ['POST'],
+    handler,
+  })
+);
