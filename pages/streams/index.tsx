9a2c135ca4aa278b8108664ef0add3017f0f9baf
rovin0805 <qhdrn67@naver.com>
2022-03-24 10:49:14 +0900

    [LIVE STREAMING] Video and Thumbnails

--- pages/streams/index.tsx
+++ pages/streams/index.tsx
@@ -4,6 +4,7 @@ import FloatingButton from '@components/floating-button';
 import Layout from '@components/layout';
 import { Stream } from '@prisma/client';
 import useSWR from 'swr';
+import Image from 'next/image';
 
 interface StreamsResponse {
   ok: boolean;
@@ -16,10 +17,16 @@ const Streams: NextPage = () => {
   return (
     <Layout hasTabBar title='라이브'>
       <div className=' space-y-4 divide-y-[1px]'>
-        {data?.streams?.map((stream) => (
+        {data?.streams?.map(stream => (
           <Link key={stream.id} href={`/streams/${stream.id}`}>
             <a className='block px-4  pt-4'>
-              <div className='aspect-video w-full rounded-md bg-slate-300 shadow-sm' />
+              <div className='relative aspect-video w-full overflow-hidden rounded-md bg-slate-300 shadow-sm'>
+                <Image
+                  layout='fill'
+                  src={`https://videodelivery.net/${stream.cloudflareId}/thumbnails/thumbnail.jpg?height=320`}
+                  alt='thumbnail'
+                />
+              </div>
               <h1 className='mt-2 text-2xl font-bold text-gray-900'>
                 {stream.name}
               </h1>
@@ -32,14 +39,12 @@ const Streams: NextPage = () => {
             fill='none'
             stroke='currentColor'
             viewBox='0 0 24 24'
-            xmlns='http://www.w3.org/2000/svg'
-          >
+            xmlns='http://www.w3.org/2000/svg'>
             <path
               strokeLinecap='round'
               strokeLinejoin='round'
               strokeWidth='2'
-              d='M15 10l4.553-2.276A1 1 0 0121 8.618v6.764a1 1 0 01-1.447.894L15 14M5 18h8a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v8a2 2 0 002 2z'
-            ></path>
+              d='M15 10l4.553-2.276A1 1 0 0121 8.618v6.764a1 1 0 01-1.447.894L15 14M5 18h8a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v8a2 2 0 002 2z'></path>
           </svg>
         </FloatingButton>
       </div>
