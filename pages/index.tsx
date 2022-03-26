14e5113b21501f740fcb586390621e30d1abb0e9
Choi Eun Jeong <qhdrn67@naver.com>
2022-03-26 23:44:48 +0900

    [NEXTJS DEEP DIVE] SSR + SWR

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import FloatingButton from '@components/floating-button';
 import Item from '@components/item';
 import Layout from '@components/layout';
 import useUser from '@libs/client/useUser';
-import useSWR from 'swr';
+import useSWR, { SWRConfig } from 'swr';
 import { Product } from '@prisma/client';
 import client from '@libs/server/client';
 
@@ -18,22 +18,24 @@ interface ProductsResponse {
   products: ProductWithCount[];
 }
 
-const Home: NextPage<{ products: ProductWithCount[] }> = ({ products }) => {
+const Home = () => {
   const { user, isLoading } = useUser();
-  // const { data } = useSWR<ProductsResponse>('/api/products');
+  const { data } = useSWR<ProductsResponse>('/api/products');
 
   return (
     <Layout title='홈' hasTabBar>
       <div className='flex flex-col space-y-5 divide-y'>
-        {products?.map((product) => (
-          <Item
-            id={product.id}
-            key={product.id}
-            title={product.name}
-            price={product.price}
-            hearts={product._count?.favs}
-          />
-        ))}
+        {data
+          ? data?.products?.map((product) => (
+              <Item
+                id={product.id}
+                key={product.id}
+                title={product.name}
+                price={product.price}
+                hearts={product._count?.favs || 0}
+              />
+            ))
+          : 'Loading...'}
         <FloatingButton href='/products/upload'>
           <svg
             className='h-6 w-6'
@@ -56,6 +58,23 @@ const Home: NextPage<{ products: ProductWithCount[] }> = ({ products }) => {
   );
 };
 
+const HomeWrapper: NextPage<{ products: ProductWithCount[] }> = ({
+  products,
+}) => (
+  <SWRConfig
+    value={{
+      fallback: {
+        '/api/products': {
+          ok: true,
+          products,
+        },
+      },
+    }}
+  >
+    <Home />
+  </SWRConfig>
+);
+
 // 로딩 상태없이 서버단에서 데이터를 다 받아온 후에 유저에게 한번에 보여줌.
 export async function getServerSideProps() {
   const products = await client.product.findMany({});
@@ -66,4 +85,4 @@ export async function getServerSideProps() {
   };
 }
 
-export default Home;
+export default HomeWrapper;
