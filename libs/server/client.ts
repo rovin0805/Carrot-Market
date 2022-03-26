3129d9a5871447997428690a053d5d2aea0906be
Choi Eun Jeong <qhdrn67@naver.com>
2022-03-26 14:30:52 +0900

    [NEXTJS DEEP DIVE] Middlewares

--- libs/server/client.ts
+++ libs/server/client.ts
@@ -4,7 +4,7 @@ declare global {
   var client: PrismaClient | undefined;
 }
 
-const client = global.client || new PrismaClient({ log: ['query', 'error'] });
+const client = global.client || new PrismaClient({ log: ['error'] });
 
 if (process.env.NODE_ENV === 'development') {
   global.client = client;
