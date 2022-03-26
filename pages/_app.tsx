431a5156aaa0e44da4dac131663c10090f65e2fa
Choi Eun Jeong <qhdrn67@naver.com>
2022-03-26 16:02:44 +0900

    [NEXTJS DEEP DIVE] Script Component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,9 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import Seo from '@components/seo';
 import { SWRConfig } from 'swr';
+import Script from 'next/script';
 
-// TODO: protect all pages by using useUser Hook expect enter page
-
-const fetcher = (url: string) => fetch(url).then(res => res.json());
+const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -14,6 +13,23 @@ function MyApp({ Component, pageProps }: AppProps) {
         <Seo />
         <Component {...pageProps} />
       </div>
+      {/* <Script
+        src="https://developers.kakao.com/sdk/js/kakao.js"
+        strategy="lazyOnload"
+      />
+      <Script
+        src="https://connect.facebook.net/en_US/sdk.js"
+        onLoad={() => {
+          window.fbAsyncInit = function () {
+            FB.init({
+              appId: "your-app-id",
+              autoLogAppEvents: true,
+              xfbml: true,
+              version: "v13.0",
+            });
+          };
+        }}
+      /> */}
     </SWRConfig>
   );
 }
