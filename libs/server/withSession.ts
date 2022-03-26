400b639488547acdfcc75204a62587337a5be104
Choi Eun Jeong <qhdrn67@naver.com>
2022-03-26 23:56:15 +0900

    [NEXTJS DEEP DIVE] SSR + Authentication

--- libs/server/withSession.ts
+++ libs/server/withSession.ts
@@ -1,4 +1,4 @@
-import { withIronSessionApiRoute } from 'iron-session/next';
+import { withIronSessionApiRoute, withIronSessionSsr } from 'iron-session/next';
 
 declare module 'iron-session' {
   interface IronSessionData {
@@ -13,6 +13,10 @@ const cookieOptions = {
   password: process.env.COOKIE_PASSWORD!,
 };
 
-export function withApiSession(fn: any) {
-  return withIronSessionApiRoute(fn, cookieOptions);
+export function withApiSession(handler: any) {
+  return withIronSessionApiRoute(handler, cookieOptions);
+}
+
+export function withSsrSession(handler: any) {
+  return withIronSessionSsr(handler, cookieOptions);
 }
