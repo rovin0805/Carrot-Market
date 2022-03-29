427660abb57ce6741357298dc3ec591464d21592
Choi Eun Jeong <qhdrn67@naver.com>
2022-03-29 22:56:21 +0900

    [ISR] ISR

--- pages/community/index.tsx
+++ pages/community/index.tsx
@@ -5,6 +5,7 @@ import Layout from '@components/layout';
 import useSWR from 'swr';
 import { Post, User } from '@prisma/client';
 import useCoords from '@libs/client/useCoords';
+import client from '@libs/server/client';
 
 // TODO: 404 error handling
 
@@ -17,22 +18,22 @@ interface PostWithUser extends Post {
 }
 
 interface PostsResponse {
-  ok: boolean;
+  // ok: boolean;
   posts: PostWithUser[];
 }
 
-const Community: NextPage = () => {
-  const { latitude, longitude } = useCoords();
-  const { data } = useSWR<PostsResponse>(
-    latitude && longitude
-      ? `/api/posts?latitude=${latitude}&longitude=${longitude}`
-      : null,
-  );
+const Community: NextPage<PostsResponse> = ({ posts }) => {
+  // const { latitude, longitude } = useCoords();
+  // const { data } = useSWR<PostsResponse>(
+  //   latitude && longitude
+  //     ? `/api/posts?latitude=${latitude}&longitude=${longitude}`
+  //     : null,
+  // );
 
   return (
     <Layout hasTabBar title='동네생활'>
       <div className='space-y-4 divide-y-[2px]'>
-        {data?.posts?.map(post => (
+        {posts?.map((post) => (
           <Link key={post.id} href={`/community/${post.id}`}>
             <a className='flex cursor-pointer flex-col items-start pt-4'>
               <span className='ml-4 flex items-center rounded-full bg-gray-100 px-2.5 py-0.5 text-xs font-medium text-gray-800'>
@@ -53,14 +54,16 @@ const Community: NextPage = () => {
                     fill='none'
                     stroke='currentColor'
                     viewBox='0 0 24 24'
-                    xmlns='http://www.w3.org/2000/svg'>
+                    xmlns='http://www.w3.org/2000/svg'
+                  >
                     <path
                       strokeLinecap='round'
                       strokeLinejoin='round'
                       strokeWidth='2'
-                      d='M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z'></path>
+                      d='M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z'
+                    ></path>
                   </svg>
-                  <span>궁금해요 {post._count.wondering}</span>
+                  <span>궁금해요 {post._count?.wondering}</span>
                 </span>
                 <span className='flex items-center space-x-2 text-sm'>
                   <svg
@@ -68,14 +71,16 @@ const Community: NextPage = () => {
                     fill='none'
                     stroke='currentColor'
                     viewBox='0 0 24 24'
-                    xmlns='http://www.w3.org/2000/svg'>
+                    xmlns='http://www.w3.org/2000/svg'
+                  >
                     <path
                       strokeLinecap='round'
                       strokeLinejoin='round'
                       strokeWidth='2'
-                      d='M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z'></path>
+                      d='M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z'
+                    ></path>
                   </svg>
-                  <span>답변 {post._count.answers}</span>
+                  <span>답변 {post._count?.answers}</span>
                 </span>
               </div>
             </a>
@@ -87,17 +92,29 @@ const Community: NextPage = () => {
             fill='none'
             stroke='currentColor'
             viewBox='0 0 24 24'
-            xmlns='http://www.w3.org/2000/svg'>
+            xmlns='http://www.w3.org/2000/svg'
+          >
             <path
               strokeLinecap='round'
               strokeLinejoin='round'
               strokeWidth='2'
-              d='M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z'></path>
+              d='M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z'
+            ></path>
           </svg>
         </FloatingButton>
       </div>
     </Layout>
   );
 };
+
+export async function getStaticProps() {
+  const posts = await client.post.findMany({ include: { user: true } });
+  return {
+    props: {
+      posts: JSON.parse(JSON.stringify(posts)),
+    },
+    revalidate: 20, // ISR
+  };
+}
 
 export default Community;
